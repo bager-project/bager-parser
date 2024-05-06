3425fcc4bf63069cdce531c05afa0bea070d9fba
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2024-05-06 23:25:14 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# B.A.G.E.R parser
+This is project documentation parser for B.A.G.E.R
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "B.A.G.E.R project documentation parser!\n";
+    return 0;
+}
