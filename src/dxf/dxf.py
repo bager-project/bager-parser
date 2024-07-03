b03b3798301ea07bacbbd65a98ebaabaf51ecd04
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2024-07-03 12:34:20 +0200

    Finished divisions

--- src/dxf/dxf.py
+++ src/dxf/dxf.py
@@ -30,12 +30,14 @@ class DXF:
         grid_size = 10
         all_coords = []
 
-        for line in self.lines:
-            all_coords.extend(line.coords)
-        bounds = Polygon(all_coords).bounds
+        # Create a polygon from the lines
+        polygon = self.create_polygon()
         
-        grid = self.create_divisions(bounds, grid_size)
-        self.plot_shape_and_grid(self.lines, grid)
+        # Create horizontal divisions
+        divisions = self.create_divisions(polygon, grid_size)
+        
+        # Plot the shape and the divisions
+        self.plot_shape_and_grid(polygon, divisions)
 
     def extract_entities(self) -> None:
         # Extract different types of elements
@@ -54,8 +56,15 @@ class DXF:
                 case _:
                     self.elements['UNIMPLEMENTED'].append(entity)
 
-    def create_divisions(self, bounds, division_number) -> None:
-        min_x, min_y, max_x, max_y = bounds
+    def create_polygon(self) -> Polygon:
+        all_coords = []
+        for line in self.lines:
+            all_coords.extend(line.coords)
+
+        return Polygon(all_coords)
+
+    def create_divisions(self, polygon: Polygon, division_number):
+        min_x, min_y, max_x, max_y = polygon.bounds
         y_points = np.arange(min_y, max_y, division_number)
         
         horizontal_lines = []
@@ -63,17 +72,30 @@ class DXF:
             horizontal_line = LineString([(min_x, y), (max_x, y)])
             horizontal_lines.append(horizontal_line)
         
-        return horizontal_lines
+        clipped_lines = []
+        for line in horizontal_lines:
+            clipped_line = polygon.intersection(line)
+            if not clipped_line.is_empty:
+                clipped_lines.append(clipped_line)
+        
+        return clipped_lines
     
-    def plot_shape_and_grid(self, lines, divisions):
+    def plot_shape_and_grid(self, polygon, divisions):
         fig, ax = plt.subplots()
-        for line in lines:
-            x, y = line.xy
-            ax.plot(x, y, color='black')
-
+    
+        # Plot the polygon
+        x, y = polygon.exterior.xy
+        ax.plot(x, y, color='black')
+        
+        # Plot the divisions
         for division in divisions:
-            x, y = division.xy
-            ax.plot(x, y, color='blue')
+            if isinstance(division, LineString):
+                x, y = division.xy
+                ax.plot(x, y, color='blue')
+            elif isinstance(division, Polygon):
+                for geom in division.geoms:
+                    x, y = geom.exterior.xy
+                    ax.plot(x, y, color='blue')
 
         plt.show()
 
